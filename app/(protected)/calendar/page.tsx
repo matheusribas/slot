02046f42fcd94ae85baeedfd3fd34a1e78df8935
matheusribas <matheusfeliperibas@gmail.com>
2026-09-02 16:26:53 -0300

    add shadcn

---
+++ app/(protected)/calendar/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Calendário | Slot",
+  description: "Encontre um horário para um serviço de forma rápida e prática.",
+};
+
+export default function CalendarPage() {
+  return (
+    <main className="flex flex-1 w-full">
+      <h3>Calendário</h3>
+    </main>
+  );
+}
---
+++ app/(unprotected)/login/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login | Slot",
+  description: "Encontre um horário para um serviço de forma rápida e prática.",
+};
+
+export default function LoginPage() {
+  return (
+    <main className="flex flex-1 w-full">
+      <h3>Login</h3>
+    </main>
+  );
+}
